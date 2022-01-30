4095affc57d6c315d1306b5c8fad07d70a974a32
bochkarevatat <tatianaistorik@gmail.com>
2022-01-30 20:51:15 +0400

    Finished cooker-cliker

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,18 @@
+const click = document.getElementById('clicker__counter');
+
+const cookie = document.getElementById('cookie');
+
+
+cookie.onclick = () => {
+    click.textContent = Number(click.textContent) + 1;
+
+    let res = click.textContent % 2;
+    if (res !== 0) {
+        cookie.width = "250"
+
+    } else {
+        cookie.width = "200"
+    }
+
+
+};
