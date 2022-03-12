61ac688520807bece430a287ea1187a2a5296aa5
bochkarevatat <tatianaistorik@gmail.com>
2022-03-12 21:14:11 +0400

    fixed cart, count and zero

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -4,7 +4,7 @@
 const productQuantityControl = Array.from(document.getElementsByClassName('product__quantity-control'));
 const minus = document.querySelectorAll('.product__quantity-control_dec')
 const plus = document.querySelectorAll('.product__quantity-control_inc')
-const productAdd =  Array.from(document.getElementsByClassName('product__add'));
+const productAdd = Array.from(document.getElementsByClassName('product__add'));
 const cartProducts = document.querySelector('.cart__products')
 
 // console.log(minus)
@@ -37,31 +37,31 @@ plus.forEach((item, u) => {
 
 productAdd.forEach((item, b) => {
 
-    
+
     productAdd[b].addEventListener('click', (e) => {
- 
+
         const id = item.closest('.product').dataset.id;
         const img = item.closest('.product').children[1].getAttribute('src')
         const value = item.parentElement.querySelector('.product__quantity-value').innerText;
+
+
         const cartProduct = Array.from(document.getElementsByClassName('cart__product'))
         const elemProduct = cartProduct.find((elem) => {
-                        return elem.dataset.id === id;
-                    }); 
+            return elem.dataset.id === id;
+        });
 
+        if (elemProduct) {
+
+            const valueCount = Number(elemProduct.querySelector('.cart__product-count').innerText);
+            elemProduct.querySelector('.cart__product-count').innerText = valueCount + (+value);
+        } else if (value != 0) {
             cartProducts.insertAdjacentHTML('beforeEnd', `
             <div class="cart__product" data-id="${id}">
                 <img class="cart__product-image" src="${img}">
                 <div class="cart__product-count">${value}</div>
             </div>`);
-            
-            if (elemProduct) {
-                elemProduct.remove()
-              
-            }
+        };
+
     });
 
-        
- });
- 
-
-
+});
