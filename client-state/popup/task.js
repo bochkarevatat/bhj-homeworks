587993c6863474cd07dbf729acf3fec262f6e4df
bochkarevatat <tanitabt@rambler.ru>
2022-06-24 00:22:13 +0400

    fixed task 2

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,25 +1,13 @@
 // Необходимо реализовать всплывающее окно, которое появляется только один раз
 const subscribeModal = document.querySelector('#subscribe-modal');
-function setCookie(name, value) {
-    document.cookie = name + '=' + encodeURIComponent(value);
-}
-const getCookie = (name) => {
-    const cookies = document.cookie.split('; ');
-    const cookie = cookies.find(e => e.startsWith(name + '='));
-    return cookie ? decodeURIComponent(cookie.substr((name + '=').length)) : null;
-}
-const closeModal = (e) => {
-    if (e.target.classList.contains('modal__close')) {
-        subscribeModal.classList.remove('modal_active');
-        document.cookie = "modalclose=true";
-    }
-} 
-if (getCookie('modalclose')) {
-    subscribeModal.classList.remove('modal_active');
-    subscribeModal.removeEventListener('click', closeModal);
-} else {
-    subscribeModal.classList.add('modal_active');
-}
-subscribeModal.addEventListener('click', closeModal);
+const modalPopup = document.querySelector(".modal__close");
 
-console.log(document.cookie)
+window.addEventListener("load", () => {
+    modalPopup.addEventListener("click", () => {
+        subscribeModal.classList.remove("modal_active");
+        document.cookie = true;
+    });
+    if (document.cookie == false) {
+        subscribeModal.classList.add("modal_active");
+    }
+});
