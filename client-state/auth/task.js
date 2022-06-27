2949ac34c850eb5955f58db9bb041c8fa0b9a323
bochkarevatat <tanitabt@rambler.ru>
2022-06-27 20:12:47 +0400

    fixed task 3

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -6,35 +6,33 @@ const signinBtn = document.getElementById('signin__btn');
 const welcome = document.getElementById('welcome');
 const userId = document.getElementById('user_id');
 formSign.classList.add('signin_active');
-signinBtn.addEventListener('click', sendForm);
 
-function sendForm(event) {
+signinBtn.addEventListener('click', (event) => {
     event.preventDefault();
     const formData = new FormData(form);
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
-    xhr.addEventListener('readystatechange', showData);
+    xhr.responseType = 'json';
+    const URL = form.getAttribute("action");
+    xhr.open('POST', URL);
+    xhr.addEventListener("load", () => {
+        if (xhr.response.success) {
+            localStorage.userId = xhr.response.user_id;
+            welcomeActive()
+            userId.innerText = response.user_id;
+        } else {
+            alert('Неверные логин или пароль');
+        }
+        form.reset();
+    });
     xhr.send(formData);
-    function showData(event) {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText);
-            if (response.success) {
-                localStorage.userId = response.user_id;
-                formSign.classList.remove('signin_active');
-                welcome.classList.add('welcome_active');
-                userId.innerText = response.user_id;
+});
 
-            } else {
-                alert('Неверные логин или пароль');
-            }
-        };
-    };
-};
-function init() {
+function welcomeActive() {
     if (localStorage.userId) {
         formSign.classList.remove('signin_active');
         welcome.classList.add('welcome_active');
         userId.innerText = localStorage.userId;
-    }
-}
-init();
+    };
+};
+
+
